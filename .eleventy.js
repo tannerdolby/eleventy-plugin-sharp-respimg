209e6b383849dfb28815e05771737149030466d4
Tanner <tannercdolby@gmail.com>
2021-06-05 06:45:09 -0700

    Refactoring sharp usage

--- .eleventy.js
+++ .eleventy.js
@@ -4,10 +4,8 @@ const fs = require("fs");
 module.exports = (eleventyConfig, pluginNamespace) => {
     eleventyConfig.namespace(pluginNamespace, () => {
         eleventyConfig.addShortcode("respimg", (data) => {
-            let extRegex = /[^.]+$/gm;
-            let fileRegex = /^\w+[^.]+/gm;
-            let format = data.src.match(extRegex);
-            let fileName = data.src.match(fileRegex);
+            const extRegex = /[^.]+$/gm;
+            const fileRegex = /^\w+[^.]+/gm;
             const imgFormats = [
                 "png",
                 "jpg",
@@ -25,6 +23,8 @@ module.exports = (eleventyConfig, pluginNamespace) => {
                 "width",
                 "height"
             ];
+            let format = data.src.match(extRegex);
+            let fileNameArr = data.src.match(fileRegex);
 
             for (const prop of reqArgs) {
                 if (typeof data[prop] == 'undefined') {
@@ -42,175 +42,127 @@ module.exports = (eleventyConfig, pluginNamespace) => {
                         return typeof w == 'string' ? parseInt(w, 10) : w;
                     });
 
-            if (widths.length < 2 && widths.length) {
-                throw new Error("The `widths` array expects atleast 3 string values");
+            if (widths.length && widths.length > 11) {
+                console.errror("The `widths` array cannot have more than 10 values");
+                return;
             }
             
             function bytesToKB(bytes) {
                 const kbRatio = 1 * Math.pow(10, -3);
                 return (bytes * kbRatio).toFixed(2);
             }
-            function smallJpeg(stream, file) {
+        
+            function jpeg(stream, file, width) {
                 return stream
                     .clone()
                     .jpeg({ quality: data.quality ? data.quality : 85 })
-                    .resize({ width: widths[0] })
-                    .toFile(`${data.inputDir}${data.imgDir}${file}-small.jpeg`)
-                    .then(res => {
-                        res.file = `${data.inputDir}${data.imgDir}${file}-small.jpeg`;
-                        res.size = `${bytesToKB(res.size)} KB`;
-                        res.quality = data.quality;
-                        data.debug ? console.log(res) : null;
+                    .resize({ width: width })
+                    .toFile(`${data.inputDir}${data.imgDir}${file}-${width}.jpeg`)
+                    .then((sharpObj) => {
+                        sharpObj.file = `${data.inputDir}${data.imgDir}${file}-${width}.jpeg`;
+                        sharpObj.size = `${bytesToKB(sharpObj.size)} KB`;
+                        sharpObj.quality = data.quality ? data.quality : 85;
+                        data.debug ? console.log(sharpObj) : null;
                     })
-                    .catch(err => {
-                        console.log(`Error transforming ${data.inputDir}${data.imgDir}${data.src} to a small sized JPEG`, err);
-                    })
-            };
-            function medJpeg(stream, file) {
-                return stream
-                    .clone()
-                    .jpeg({ quality: data.quality ? data.quality : 85 })
-                    .resize({ width: widths[1] })
-                    .toFile(`${data.inputDir}${data.imgDir}${file}-med.jpeg`)
-                    .then(res => {
-                        res.file = `${data.inputDir}${data.imgDir}${file}-med.jpeg`;
-                        res.size = `${bytesToKB(res.size)} KB`;
-                        res.quality = data.quality;
-                        data.debug ? console.log(res) : null;
-                    })
-                    .catch(err => {
-                        console.log(`Error transforming ${data.inputDir}${data.imgDir}${data.src} to a medium sized JPEG`, err);
+                    .catch((err) => {
+                        console.log(`Error transforming ${data.inputDir}${data.imgDir}${data.src}`, err);
                     })
             };
-            function largeJpeg(stream, file) {
-                return stream  
-                    .clone()
-                    .jpeg({ quality: data.quality ? data.quality : 85 })
-                    .resize({ width: widths[2] })
-                    .toFile(`${data.inputDir}${data.imgDir}${file}-large.jpeg`)
-                    .then(res => {
-                        res.file = `${data.inputDir}${data.imgDir}${file}-large.jpeg`;
-                        res.size = `${bytesToKB(res.size)} KB`;
-                        res.quality = data.quality;
-                        data.debug ? console.log(res) : null;
-                    })
-                    .catch(err => {
-                        console.log(`Error transforming ${data.inputDir}${data.imgDir}${data.src} to a large JPEG format`, err);
-                    })
-            };
-            function smallWebp(stream, file) {
+
+            function webp(stream, file, width) {
                 return stream
                     .clone()
                     .webp({ quality: data.quality ? data.quality : 85 })
-                    .resize({ width: widths[0] })
-                    .toFile(`${data.inputDir}${data.imgDir}${file}-small.webp`)
-                    .then(res => {
-                        res.file = `${data.inputDir}${data.imgDir}${file}-small.webp`;
-                        res.size = `${bytesToKB(res.size)} KB`;
-                        res.quality = data.quality;
-                        data.debug ? console.log(res) : null;
+                    .resize({ width: width })
+                    .toFile(`${data.inputDir}${data.imgDir}${file}-${width}.webp`)
+                    .then((sharpObj) => {
+                        sharpObj.file = `${data.inputDir}${data.imgDir}${file}-${width}.webp`;
+                        sharpObj.size = `${bytesToKB(sharpObj.size)} KB`;
+                        sharpObj.quality = data.quality ? data.quality : 85;
+                        data.debug ? console.log(sharpObj) : null;
                     })
-                    .catch(err => {
-                        console.log(`Error transforming ${data.inputDir}${data.imgDir}${data.src} to small WebP format`, err);
-                    })
-            };
-            function medWebp(stream, file) {
-                return stream
-                    .clone()
-                    .webp({ quality: data.quality ? data.quality : 85 })
-                    .resize({ width: widths[1] })
-                    .toFile(`${data.inputDir}${data.imgDir}${file}-med.webp`)
-                    .then(res => {
-                        res.file = `${data.inputDir}${data.imgDir}${file}-med.webp`;
-                        res.size = `${bytesToKB(res.size)} KB`;
-                        res.quality = data.quality;
-                        data.debug ? console.log(res) : null;
-                    })
-                    .catch(err => {
-                        console.log(`Error transforming ${data.inputDir}${data.imgDir}${data.src} to medium sized webp`, err);
+                    .catch((err) => {
+                        console.log(`Error transforming ${data.inputDir}${data.imgDir}${data.src}`, err);
                     })
             };
-            function largeWebp(stream, file) {
-                return stream
-                    .clone()
-                    .webp({ quality: data.quality ? data.quality : 85 })
-                    .resize({ width: widths[2] })
-                    .toFile(`${data.inputDir}${data.imgDir}${file}-large.webp`)
-                    .then(res => {
-                        res.file = `${data.imgDir}${file}-large.webp`;
-                        res.size = `${bytesToKB(res.size)} KB`;
-                        res.quality = data.quality;
-                        data.debug ? console.log(res) : null;
-                    })
-                    .catch(err => {
-                        console.log(`Error transforming ${data.inputDir}${data.imgDir}${data.src} to small sized webp`, err);
-                    })
-            };
+
             function transform(img) {
-                let filename = img.match(fileRegex);
-                let parentStream = sharp(`${data.inputDir}${data.imgDir.concat(img)}`);
-                
-                smallJpeg(parentStream, filename[0]);
-                medJpeg(parentStream, filename[0]);
-                largeJpeg(parentStream, filename[0]);
-                smallWebp(parentStream, filename[0]);
-                medWebp(parentStream, filename[0]);
-                largeWebp(parentStream, filename[0]);
+                let filenameArr = img.match(fileRegex);
+                let filename = filenameArr[0];
+                let stream = sharp(`${data.inputDir}${data.imgDir.concat(img)}`);
+
+                widths.forEach((width) => {
+                    jpeg(stream, filename, width);
+                    webp(stream, filename, width);
+                });
+            };
+
+            function markupGenerator(widths) {
+                let filename = fileNameArr[0];
+                let imgSrcSet = "";
+                let webpSrcSet = "";
+
+                widths.forEach((width) => {
+                    imgSrcSet += `${data.imgDir}${filename}-${width}.jpeg ${width}w, `;
+                    webpSrcSet += `${data.imgDir}${filename}-${width}.webp ${width}w, `;
+                });
+
+                const img = 
+                `<img 
+                    srcSet="${imgSrcSet.slice(0, imgSrcSet.length - 2)}"
+                    sizes="${data.sizes}"
+                    src="${data.imgDir}${fileNameArr}-${data.fallbackSrcWidth || widths[0]}.jpeg"
+                    alt="${data.alt}"
+                    loading="lazy"
+                    class="${data.className ? data.className : ''}"
+                    width="${data.width}"
+                    height="${data.height}" ${data.id ? `id='${data.id}'` : ''}>`;
+                const picture = 
+                `<picture>
+                    <source 
+                        type="image/webp"
+                        srcSet="${webpSrcSet.slice(0, imgSrcSet.length - 2)}"
+                        sizes="${data.sizes}"
+                    >
+                    ${img}
+                </picture>`;
+
+                return picture;
+            };
+
+            function generator(widths) {
+                let fileObj = {};
+                let filename = fileNameArr[0];
+                widths.forEach((width) => {
+                    fileObj[`${width}_jpeg`] = `${data.inputDir}${data.imgDir}${filename}-${width}.jpeg`;
+                    fileObj[`${width}_webp`] = `${data.inputDir}${data.imgDir}${filename}-${width}.webp`;
+                });
+                return fileObj;
             }
 
-            const imgMarkup = 
-            `<img 
-                srcSet="${data.imgDir}${fileName}-large.jpeg ${data.widths[2]}w,
-                ${data.imgDir}${fileName}-med.jpeg ${data.widths[1]}w,
-                ${data.imgDir}${fileName}-small.jpeg ${data.widths[0]}w"
-                sizes="${data.sizes}"
-                src="${data.imgDir}${fileName}-small.jpeg"
-                alt="${data.alt}"
-                loading="lazy"
-                ${data.id ? `id='${data.id}'` : ''}
-                class="${data.className ? data.className : ''}"
-                width="${data.width}"
-                height="${data.height}">`;
-            const pictureMarkup =
-            `<picture>
-                <source 
-                    type="image/webp"
-                    srcSet="${data.imgDir}${fileName}-large.webp ${data.widths[2]}w,
-                    ${data.imgDir}${fileName}-med.webp ${data.widths[1]}w,
-                    ${data.imgDir}${fileName}-small.webp ${data.widths[0]}w"
-                    sizes="${data.sizes}"
-                >
-                ${imgMarkup}
-            </picture>`;
-            
-            const files = {
-                large_jpg: `${data.inputDir}${data.imgDir}${fileName}-large.jpeg`,
-                med_jpg: `${data.inputDir}${data.imgDir}${fileName}-med.jpeg`,
-                small_jpg: `${data.inputDir}${data.imgDir}${fileName}-small.jpeg`,
-                large_webp: `${data.inputDir}${data.imgDir}${fileName}-large.webp`,
-                med_webp: `${data.inputDir}${data.imgDir}${fileName}-med.webp`,
-                small_webp: `${data.inputDir}${data.imgDir}${fileName}-small.webp`
-            };
+            const html = markupGenerator(widths);
+            const files = generator(widths);
+            const fileArrSize = Object.keys(files).length;
+            let srcFileExists = fs.existsSync(`${data.inputDir}${data.imgDir.concat(data.src)}`);
+            let fileExistsCount = 0;
 
-            let fileExists = fs.existsSync(`${data.inputDir}${data.imgDir.concat(data.src)}`);
-            let i = 0;
-
-            if (fileExists) { 
+            if (srcFileExists) { 
                 for (const name in files) {
                     if (fs.existsSync(files[name])) {
-                        i += 1;
+                        fileExistsCount += 1;
                     }
                 }
-                if (!data.overwrite && i !== 6) {
+                if (
+                    !data.overwrite && fileExistsCount !== fileArrSize || 
+                    data.overwrite && fileExistsCount == fileArrSize
+                ) {
                     transform(data.src);
+                    console.log(`Generating responsive images for '${data.inputDir}${data.imgDir}${data.src}'`);
                 }
-                if (data.overwrite && i == 6) {
-                    transform(data.src);
-                } 
             } else {
                 throw new Error(`Image doesn't exist! Cannot locate ${data.inputDir}${data.imgDir.concat(data.src)}`);
             }
-            return pictureMarkup;
+            return html;
         });
    });
 };
